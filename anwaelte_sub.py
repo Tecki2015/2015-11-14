c896527f68b7ed343d65d1a748de2547453753c5
Tecki2015 <github@tecki.org>
2015-11-14 07:59:43 +0100

    New Commit to NewBranch

---
+++ anwaelte_sub.py
@@ -0,0 +1,124 @@
+﻿# -*- coding: utf-8 -*-
+import urllib.request
+import string
+from bs4 import BeautifulSoup
+#import re
+import csv
+import os
+import codecs
+import subprocess
+
+#import time
+#from selenium import webdriver
+#from selenium.webdriver.common.by import By
+#from selenium.webdriver.support.ui import WebDriverWait
+#from selenium.webdriver.support import expected_conditions as EC
+#from selenium.webdriver.common.keys import Keys
+
+datei = "anwaelteC.txt"
+path = "output2.csv"
+
+
+#remove existing outputfile
+os.remove(path)
+
+# Open the file in R/W and create if it doesn't exist. 
+os.open(path, os.O_RDWR | os.O_CREAT)
+
+#write expliced unicode bom in file:
+f = open(path, "wb")
+f.write(codecs.BOM_UTF8)
+
+
+#path notepad++
+pnpp = "C:/Program Files (x86)/Notepad++/Notepad++.exe"
+
+
+
+#print (os.path.dirname(os.path.realpath(__file__)))
+
+
+
+
+
+
+def csv_writer(data, path):
+        with open(path, 'a', encoding='utf8', newline='') as csv_file:
+                writer = csv.writer(csv_file, delimiter=';', quotechar = '"', quoting=csv.QUOTE_ALL)
+                writer.writerow(data)
+            #for line in data:
+                #writer.writerow(line)
+
+
+
+
+
+
+
+buchstabe = None
+alpha = list(string.ascii_uppercase)
+alphaZ = 0
+
+with open(datei) as f:
+    links = f.read().splitlines()
+
+#print(links)
+
+for asite in links:
+        source = urllib.request.urlopen(asite)
+        html = source.read()
+        tree = BeautifulSoup(html, "lxml")
+        g_data = tree.find_all("div", {"id": "viewsegmentB"})
+        for item in g_data:
+               
+                try:
+                        Titel = (item.find_all("strong", {"itemprop": "name"})[0]).text.split(" ")[-3]
+                except:
+                        Titel = ""
+                try:
+                        Vorname = (item.find_all("strong", {"itemprop": "name"})[0]).text.split(" ")[-2]
+                except:
+                        Vorname = ""
+                
+                Nachname = (item.find_all("strong", {"itemprop": "name"})[0]).text.split(" ")[-1]
+                try:
+                        jobTitel = (item.find_all("span", {"itemprop": "jobTitle"})[0]).text
+                except:
+                        jobTitel = ""
+
+                FullName =  Vorname = (item.find_all("strong", {"itemprop": "name"})[0]).text
+
+
+
+                Legal = (item.find_all("strong", {"itemprop": "legalName"})[0]).text
+                Strasse = (item.find_all("p", {"itemprop": "streetAddress"})[0]).text
+                PLZ = (item.find_all("span", {"itemprop": "postalCode"})[0]).text
+                Ort = (item.find_all("span", {"itemprop": "addressLocality"})[0]).text
+                Tele = (item.find_all("span", {"itemprop": "telephone"})[0]).text
+                try:
+                        eMail = (item.find_all("a", {"itemprop": "email"})[0]).text
+                except:
+                        eMail = ""
+                try:
+                        inet = (item.find_all("a", {"itemprop": "url"})[0]).text
+                except:
+                        inet = ""
+
+
+                block = [Legal, Titel, FullName, Vorname, Nachname, jobTitel, Strasse, PLZ, Ort, Tele, eMail, inet]
+                print(block)
+            
+                
+
+                csv_writer(block, path) 
+
+
+                #stuff = (Legal + ";" + Titel + ";" + FullName + ";" + Vorname + ";" + Nachname + ";" + jobTitel + ";" + Strasse + ";" + PLZ + ";" + Ort + ";" + Tele + ";" + eMail+ ";" + inet)
+                
+                
+                #with open (csv, "a") as fh:
+                        #fh.write(stuff + "\n")
+
+# open notepadd ++ with file
+subprocess.Popen([pnpp, path])
+              
---
+++ anwaelte_vs2015.py
@@ -0,0 +1,108 @@
+﻿import urllib.request
+import string
+from bs4 import BeautifulSoup
+from selenium import webdriver
+import re
+
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
+
+datei = "anwaelte.txt"
+buchstabe = None
+alpha = list(string.ascii_uppercase)
+alphaZ = 0
+
+
+def writeStuff():
+        print (buchstabe)
+        url = "http://www.anwaltsregister.de/Anwaelte_aus_Deutschland/Nachnamen_beginnend_mit_%s.html" %buchstabe
+        browser = webdriver.Firefox()
+        browser.get(url)
+        #time.sleep(5)
+        html = browser.page_source
+        tree = BeautifulSoup(html)
+        
+        
+        
+        g_data = tree.find_all("nav", {"id": "attorneylistLinks"})
+        #f = open("test.txt", "w")
+        #f.write(str(g_data))
+        #f.close()
+        #browser.quit() 
+
+        for item in g_data:
+                 name = ((item.find_all("span", {"class": "displayName"})[0]).text)
+                 print(name)
+                 #address = ((item.find_all("div", {"itemtype": "http://schema.org/LocalBusiness"})[0]).text.replace("\n",""))
+                 #sep = ' | '
+                 #address = address.split(sep, 1)[0]
+                 #try:
+                 #        tel = ((item.find_all("span", {"itemprop": "telephone"})[0]).text)
+                 #except:
+                 #        tel = "NA"
+                 #try:
+                 #        contact1 = ((item.find_all("a", {"class": "search-website searchActionLinks"})[0]["href"]))
+                 #except:
+                 #        contact1 = "NA"
+                        
+                 #try:
+                 #        rating = ((item.find_all("a", {"class": "ratingStarSearch"})[0]).text)
+                 #except:
+                 #        rating = "NA"
+
+                 #stuff = (name + ";" + address + ";" + tel + ";" + contact1 + ";" + rating)
+                 #stuff = str(stuff)
+                 #stuff = re.sub(' +',' ',stuff)
+                 #stuff = stuff.replace("\r","")
+                 #stuff = stuff.replace("\t","")
+                 #stuff = "".join(stuff.splitlines())
+                 #print (stuff)
+                 #with open (datei, "a") as fh:
+                 #        fh.write(stuff + "\n")
+                        
+                 
+                 
+        #print (g_data)
+        browser.quit()        
+
+
+
+for i in range(1):
+        buchstabe = alpha[0]
+        print(i)
+        writeStuff()
+        alphaZ = alphaZ + 1
+
+
+
+
+ browser = webdriver.Firefox()
+        browser.get(url)
+        html = browser.page_source
+        tree = BeautifulSoup(html)
+
+        
+        g_data = tree.find_all("a")
+        print(len(g_data))
+        counter = 0
+        
+        for item in g_data:
+                 
+                 if "Rechtsanw" in item.text:
+                         link = (item['href'])
+
+                         print (item['href'])
+                         with open (datei, "a") as fh:
+                                 fh.write(link + "\n")
+
+                         
+                 
+               
+        browser.quit()        
+
+
+
